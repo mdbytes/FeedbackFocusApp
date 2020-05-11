9eedf6a3eced1d1dfa134d70f1e4e968db4ef89e
martindwyer <martin.dwyer@bdo-tech.com>
2020-05-11 12:08:22 -0500

    code updates

--- config/prod.js
+++ config/prod.js
@@ -4,4 +4,6 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  callbackURL:
+    "https://customer-email-response-app.herokuapp.com/auth/google/callback",
 };
