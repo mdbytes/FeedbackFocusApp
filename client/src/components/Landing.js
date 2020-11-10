cb906b0e99bd0fd9f4aec1b8a969107564fce6ec
martindwyer <martin.dwyer@bdo-tech.com>
2020-11-10 11:28:12 -0600

    final commitfor course work

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,7 +4,7 @@ const Landing = () => {
   return (
     <div style={{ textAlign: "center" }}>
       <h1>FeedbackFocus</h1>
-      Collect feedback from your users.
+      <p>Collect feedback from your users.</p>
     </div>
   );
 };
